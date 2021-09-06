0ab943674362bbd8ee8075d7f84df38c491b74e4
A01661455 <89362416+A01661455@users.noreply.github.com>
2021-09-05 20:45:52 -0500

    Update exercise.py

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -1,8 +1,19 @@
-
-
+#Carlos Peña Gutiérrez A01661455
 def main():
     #Escribe tu código debajo de esta línea
     pass
+
+n=int(input("Escribe un número:"))
+
+inicial=1
+
+while inicial**2<=n:
+        inicial=inicial+1
+        
+else:
+        print(inicial)
+   
+     
 
 if __name__=='__main__':
     main()
