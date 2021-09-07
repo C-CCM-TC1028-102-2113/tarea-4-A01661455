c533ca6491e9ec1b294a4ea3fa26533bfa9f8f8a
A01661455 <89362416+A01661455@users.noreply.github.com>
2021-09-07 16:47:42 -0500

    Update exercise.py

--- assignments/06PromedioConDecision/src/exercise.py
+++ assignments/06PromedioConDecision/src/exercise.py
@@ -1,3 +1,19 @@
+#Carlos Peña Gutiérrez A01661455
+
+n=0
+suma=0 
+cuenta=0
+total=0
+
+while n>=0:
+ n=float(input())
+ if n>0:
+    suma=suma+n
+    cuenta=cuenta+1
+ elif n<0:
+    prom= suma/cuenta
+    print(prom)
+   
 def main():
     #escribe tu código abajo de esta línea
     pass
