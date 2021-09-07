2a13ac086889ff3b5543eb8b1385cdfc369eb549
A01661455 <89362416+A01661455@users.noreply.github.com>
2021-09-07 16:14:05 -0500

    Update exercise.py

--- assignments/28Fibonacci/src/exercise.py
+++ assignments/28Fibonacci/src/exercise.py
@@ -1,1 +1,26 @@
+#Carlos Peña Gutiérrez A01661455
 
+n=int(input("Enter a number: "))
+
+a=0
+b=1
+inicial=1
+suma=0
+
+while inicial<=n:
+ if n>=0:
+    inicial+=1
+    a=b
+    b=suma
+    suma=a+b
+ else: 
+     continue
+
+print(suma)
+
+def main():
+    #escribe tu código abajo de esta línea
+    pass
+
+if __name__=='__main__':
+    main()
